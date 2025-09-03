176ea82769f46ce5cc9351e758775174d3b50090
peerawattae <peerawat.thee@ku.th>
2025-09-03 16:52:56 +0700

    register class for admin site

--- backend/plans/admin.py
+++ backend/plans/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import users, plans, tags, Participants, reviews, chat_member, chat_messages, chat_threads
 # Register your models here.
+admin.site.register(users)
+admin.site.register(plans)
+admin.site.register(tags)
+admin.site.register(Participants)
+admin.site.register(reviews)
+admin.site.register(chat_member)
+admin.site.register(chat_messages)
+admin.site.register(chat_threads)
