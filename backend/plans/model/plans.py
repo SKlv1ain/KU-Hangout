3222f21537fd09ecf5c30b66527ce981ad6e30f6
peerawattae <peerawat.thee@ku.th>
2025-09-03 17:51:48 +0700

    reorganize model fiels to folder

---
+++ backend/plans/model/plans.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from users import users
+
+# Model for plans
+class plans(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    location = models.CharField(max_length=100)
+    lat = models.DecimalField(decimal_places=2, max_digits=8, default=None, null=True, blank=True)
+    lng = models.DecimalField(decimal_places=2, max_digits=8, default=None, null=True, blank=True)
+    leader_id = models.ForeignKey(users, related_name="leader")
+    event_time = models.DateTimeField()
+    max_people = models.IntegerField(default=1)
+    tags = models.ManyToManyField(tags, related_name='plans')
+    people_joined = models.IntegerField(default=1)
+    create_at = models.DateTimeField(auto_now_add=True)
---
+++ backend/plans/model/users.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+
+# Model for users
+class users(AbstractUser):
+    ROLE_CHOICES = [
+    ('user', 'User'),
+    ('leader', 'Leader'),
+    ('participant', 'Participant')
+]
+    username = models.CharField(max_length=50)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    avg_rating = models.DecimalField(max_digits=4, decimal_places=2)
+    review_count = models.FloatField(max_length=100)
+    contact = models.CharField(max_length=100)
+    create_at = models.DateTimeField(auto_now_add=True)
