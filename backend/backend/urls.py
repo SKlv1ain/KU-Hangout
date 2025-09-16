249143594561bdffd71abf8c2eb3bde6d51e0fdf
Sai Khun Main <saikhunmain.s@ku.th>
2025-09-16 18:07:01 +0700

    modify path from accounts to urls path of backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,16 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import RegisterView, LoginView, MeView, LogoutView
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    path('api/auth/register', RegisterView.as_view(), name='auth-register'),
-    path('api/auth/login', LoginView.as_view(), name='auth-login'),
-    path('api/auth/logout', LogoutView.as_view(), name='auth-logout'),
     
-    # Current user
-    path('api/users/me', MeView.as_view(), name='users-me'),
-
+    # Auth
+    path("api/", include("accounts.urls")),
 ]
