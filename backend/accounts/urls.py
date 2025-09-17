f3f6d2e36134133797f8e37b6ae23449e80080eb
Sai Khun Main <saikhunmain.s@ku.th>
2025-09-17 20:41:26 +0700

    Removed comments

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,11 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('register/', views.RegisterView.as_view(), name='register'),
-    # path('login/', views.LoginView.as_view(), name='login'),
-    # path('me/', views.MeView.as_view(), name='me'),
-    # path('logout/', views.LogoutView.as_view(), name='logout'),
-    
     path("auth/register", views.RegisterView.as_view(), name="auth-register"),
     path("auth/login", views.LoginView.as_view(), name="auth-login"),
     path("auth/logout", views.LogoutView.as_view(), name="auth-logout"),
