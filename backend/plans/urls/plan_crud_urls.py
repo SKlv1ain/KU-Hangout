d33b636970878094b37195e175c01d3d5bf9c48d
peerawattae <peerawat.thee@ku.th>
2025-11-13 00:09:28 +0700

    rename path plan detail in plan_crud to plan update

--- backend/plans/urls/plan_crud_urls.py
+++ backend/plans/urls/plan_crud_urls.py
@@ -3,5 +3,5 @@ from plans.views.plan_creation import PlansCreate
 
 urlpatterns = [
     path('create/', PlansCreate.as_view(), name='create-plan'),       # POST new plan
-    path('<int:pk>/', PlansCreate.as_view(), name='plan-detail'),     # PUT, DELETE plan by ID
+    path('<int:pk>/', PlansCreate.as_view(), name='plan-update'),     # PUT, DELETE plan by ID
 ]
